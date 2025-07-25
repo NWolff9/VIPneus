bc337ccaf0097a1cbe5c37c5771b69aab95f9319
NWolff9 <natha.wolff9@gmail.com>
2025-07-25 09:53:31 -0300

    backend

---
+++ Backend/vipneus/src/main/java/com/backend/vipneus/VipneusApplication.java
@@ -0,0 +1,13 @@
+package com.backend.vipneus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VipneusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VipneusApplication.class, args);
+	}
+
+}
---
+++ Backend/vipneus/src/test/java/com/backend/vipneus/VipneusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.vipneus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VipneusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
